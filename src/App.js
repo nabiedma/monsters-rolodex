1e7435be4f488e01460b46350bd6b004ff8d5eea
alebiedma <alejandro@joinqoala.com>
2022-11-02 15:29:58 -0300

    add filter search and optimizations in code

--- src/App.js
+++ src/App.js
@@ -9,10 +9,14 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: ''
     }
+
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then((response) => response.json())
     .then((users) => this.setState(() => {
@@ -22,11 +26,33 @@ class App extends Component {
     }));
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(
+      () => {
+        return { searchField };
+      }
+    );
+  }
+
   render() {
+    console.log('render');
+
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
+
     return (
       <div className="App">
+        <input className='search-box' 
+        type="search" 
+        placeholder='search monsters...' 
+        onChange={onSearchChange}
+        />
         {
-          this.state.monsters.map((monster) => { 
+          filteredMonsters.map((monster) => { 
             return <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
